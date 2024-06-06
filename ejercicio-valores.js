eb6dea353f326aa724530c7b5c92ed4c2e9122b3
Engelbert <engelfreites97@gmail.com>
2024-06-06 01:25:48 -0500

    ejercicio valores

---
+++ ejercicio-valores.js
@@ -0,0 +1,9 @@
+//En una variable tienes el lado de un cuadrado,
+//debes escribir un programa que te calcule el área y el perímetro del cuadrado.
+//El area la calculas como lado multiplicado por lado. El perímetro es la suma de los cuatro lados
+
+let lado = 40;
+let are;
+let perimetro;
+
+// Respuesta
