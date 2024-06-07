525e247115b1ebababe0dc350ef7bc6ff3d562c2
Engelbert <engelfreites97@gmail.com>
2024-06-06 22:45:44 -0500

    expresionAndDeclaracion

---
+++ expresiones-and-declaration/ejercicios.js
@@ -0,0 +1,51 @@
+// ! EJERCICIO 1
+/*Vamos a trabajar un poco con la consola para ir amigandonos con ella ya que será nuestra mejor amiga de ahora en más.
+1.-Declara una variable llamada nombre en tu consola y asignale tu nombre.
+2.-Declara una variable llamada apellido y asignale como valor tu apellido.
+3.-Muestra por consola las variables nombre y apellido. 
+*/
+
+// ! EJERCICIO 2
+
+/*Declara las variables nombre y apellido que habiamos declarado en la consola. 
+Pero, esta vez deberemos asignarle el método prompt() para que sea el usuario quien introduzca los datos, nombre y apellido.
+Mostrar una alerta de bienvenida a nuestra página con el nombre y el apellido del usuario 
+“Bienvenido a nuestra página (nombre+apellido)”*/
+
+// ! EJERCICIO 3
+/* 
+1.-Crea 3 variables numero1,numero2 y resultado.
+2.-Pidele al usuario que ingrese los valores de las variables numero1 y numero2 mediante un prompt().
+3.-Asigna a la variable resultado el valor de la suma entre numero1 y numero2.
+4.-Muestra un alerta con el mensaje “El resultado es: (resultado)”. 
+*/
+
+// ! EJERCICIO 4
+
+/*
+1.-Crea 2 variables numero y numerotriple.
+2.-Pidele al usuario que ingrese el valor del numero a calcular mediante un prompt().
+3.-Asigna a la variable numerotriple el valor de la multiplicación por 3 del numero ingresado.
+4.-Muestralo el resultado por consola con el mensaje “El triple de este número es: (xx)”.
+*/
+
+//?                                                   CONDICIONALES
+
+// ! EJERCICIO 1
+
+/*
+Mi primer condicional
+
+1.-Realizar un algoritmo que pida el nombre de una persona. Si el dato entra vacío, 
+debe pedir nuevamente al usuario que introduzca su nombre (“No ha introducido ningún nombre”).
+2.-Una vez hecho esto, que se despliegue una alerta de bienvenida con el nombre del usuario.
+
+*/
+
+//! EJERCICIO 2
+
+/* 
+1.-Pedirle al usuario que introduzca 1 numero.
+2.-Comparar si es par o no, a través de un if/else. Si es par que muestre una alerta indicando 
+que el número es par o en caso contrario que muestre una alerta que es impar.
+*/
---
+++ expresiones-and-declaration/index.js
@@ -0,0 +1,87 @@
+//?Una expresión es cualquier pedazo de código que genera un valor.
+//?Las expresiones pueden ser sencillas o complicadas, y se usan para obtener un resultado.
+
+42; // Una expresión que evalúa a 42
+("Hola"); // Una expresión que evalúa a la cadena "Hola"
+true; // Una expresión que evalúa a true
+
+//?Operaciones Aritméticas: Sumas, restas, multiplicaciones, divisiones, etc.
+
+5 + 3; // Una expresión que evalúa a 8
+10 - 2; // Una expresión que evalúa a 8
+
+//?Para atrapar y retener valores, JavaScript proporciona una cosa llamada un enlace, o variable:
+
+let caught = 5 * 5;
+
+//?Después de que se haya definido un enlace, su nombre se puede usar como una expresión.
+
+let ten = 10;
+console.log(ten * ten);
+// → 100
+
+//?Cuando un enlace apunta a un valor, eso no significa que esté atado a ese valor para siempre.
+
+let mood = "light";
+console.log(mood);
+// → light
+mood = "dark";
+console.log(mood);
+// → dark
+
+//?hay una serie de palabras que están “reservadas para su uso” en futuras versiones de JavaScript,
+//?las cuales tampoco se pueden usar como nombres de enlaces.
+
+/*break case catch class const continue debugger default
+delete do else enum export extends false finally for
+function if implements import interface in instanceof let
+new package private protected public return static super
+switch this throw true try typeof var void while with yield*/
+
+// ! Control de Flujo
+
+//?Cuando tu programa contiene más de una sentencia, las sentencias se ejecutan como si fueran una historia, de arriba hacia abajo.
+
+let elNumero = Number(prompt("Elige un número")); //pedimos un numero
+
+console.log("Tu número es la raíz cuadrada de " + elNumero * elNumero); //esto se ejecuta luego de la primera
+
+// ! Ejecución condicional
+//? La ejecución condicional se crea con la palabra clave if en JavaScript.
+
+/*La ejecución condicional se crea con la palabra clave if en JavaScript. 
+En el caso simple, queremos que cierto código se ejecute si, y solo si,
+una cierta condición es verdadera.  */
+
+let elNumero2 = Number(prompt("Elige un número"));
+if (typeof elNumero != "string") {
+  console.log("Tu número es la raíz cuadrada de " + elNumero2 * elNumero2);
+}
+
+//Si tienes más de dos caminos para elegir, puedes “encadenar” múltiples pares if/else. Aquí tienes un ejemplo:
+
+let num = Number(prompt("Escoge un número"));
+
+if (num < 10) {
+  console.log("Pequeño");
+} else if (num < 100) {
+  console.log("Mediano");
+} else {
+  console.log("Grande");
+}
+
+// Bucle while
+
+//?es una manera de ejecutar un fragmento de código múltiples veces. Esta forma de control de flujo se llama bucle.
+
+/*El control de flujo mediante bucles nos permite regresar a algún punto 
+en el programa donde estábamos antes y repetirlo con nuestro estado de programa actua */
+
+let numero = 0;
+while (numero <= 12) {
+  console.log(numero);
+  numero = numero + 2;
+}
+// → 0
+// → 2
+//   … etcétera
